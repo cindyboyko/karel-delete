972b6086a636ba5b68789791a69ec8345f1a5f11
cindyboyko <cboyko@auhsdschools.org>
2017-08-14 14:01:23 -0700

    File Setup
    Initial Files

---
+++ new-delete/Trial.java
@@ -0,0 +1,34 @@
+
+/**
+ * Write a description of class Trial here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Trial
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Trial
+     */
+    public Trial()
+    {
+        // initialise instance variables
+        x = 0;
+        System.out.println (sampleMethod(12));
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
